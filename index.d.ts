1a5f0db883a111b67d78ed90e46abd0f22cd0fe8
takanopontaro <takano@superhero.jp>
2018-08-28 16:46:27 +0900

    wow

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type TInfo = {
-    path: string;
-    byte: number;
-    done: boolean;
-};
-export declare type TCallback = (info: TInfo) => void;
-declare const _default: (cwd: string, paths: string[], dest: string, cb?: TCallback | undefined) => Promise<TInfo>;
-export default _default;
