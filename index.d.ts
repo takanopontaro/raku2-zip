bfefe71fb98b59735398913b4a58e552877d4578
takanopontaro <takano@superhero.jp>
2018-09-05 10:43:14 +0900

    wow

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare function Raku2Zip(
+  src: string | string[],
+  dest: string,
+  options?: Raku2Zip.Options | null,
+  cb?: Raku2Zip.ProgressCallback
+): Promise<Raku2Zip.ProgressData>;
+
+declare namespace Raku2Zip {
+  type Options = {
+    cwd?: string;
+    [key: string]: any;
+  };
+  type ProgressData = {
+    path: string;
+    completedSize: number;
+    done: boolean;
+  };
+  type ProgressCallback = (data: ProgressData) => void;
+}
+
+export = Raku2Zip;
